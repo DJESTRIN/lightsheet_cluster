b43745532547b5cea0a78b5cdbf00dabdd353fed
dhritimamtora <117092024+dhritimamtora@users.noreply.github.com>
2023-09-13 14:29:46 -0400

    Update convert_to_tiff.py

--- convert_to_tiff.py
+++ convert_to_tiff.py
@@ -12,7 +12,7 @@ def pngtotiff(input_dir, output_dir):
         
     #finding all the .png files
     os.chdir(input_dir)
-    files = glob.glob('*.png', recursive=True)
+    files = glob.glob('/**/*.png', recursive=True)
     
     #iterate through all files
     for file in files:
