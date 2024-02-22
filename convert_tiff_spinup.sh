32b9c6212191ced38c51fcf5a775563275739a14
DJESTRIN <david.james.estrin@outlook.com>
2024-02-21 19:42:47 -0500

    final updates to pipeline for cayuga

--- convert_tiff_spinup.sh
+++ convert_tiff_spinup.sh
@@ -7,18 +7,27 @@ store_finish_directory=$3
 #Update sample list (in the case of any issues)
 scratch_raw=${scratch_directory}"lightsheet/raw/"
 scratch_converted=${scratch_directory}"lightsheet/converted/"
-
 mkdir -p $scratch_converted
+
+# Python script replacing 0 byte files with empty images.
+for folder in $scratch_raw*/
+do
+        TMP=$(echo $folder)
+        echo $TMP
+        sbatch --job-name=remove_empty_images --mem=300G --partition=scu-cpu --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu --wrap="source ~/.bashrc && conda activate /home/fs01/dje4001/anaconda3/envs/regular && python '$code_directory'/replaceempty.py --pathway '$TMP'"
+
+done
 
 #Loop through samples
 for folder in $scratch_raw*/
 do
         TMP=$(echo $folder)
         echo $TMP
-        sbatch --job-name=convert_png_to_tiff --mem=300G --partition=scu-cpu --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu --wrap="bash /home/fs01/dje4001/lightsheet_cluster/convert.sh $code_directory $TMP"
+        sbatch --job-name=convert_png_to_tiff --mem=300G --partition=scu-cpu --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu --wrap="source ~/.bashrc && conda activate /home/fs01/dje4001/anaconda3/envs/regular && python $code_directory/convert_to_tiff.py --input_directory '$TMP' --output_directory '$scratch_converted'"
 done
 
-#sbatch --mem=50G --partition=scu-cpu --dependency=singleton --job-name=destripe_files --wrap="bash estrin_destripe_spinup.sh '$code_directory' '$scratch_directory' '$store_finish_directory'"
+sbatch --mem=50G --partition=scu-cpu --dependency=singleton --job-name=convert_png_to_tiff --wrap="bash destripe_spinup.sh '$code_directory' '$scratch_directory' '$store_finish_directory'"
+
 
 
 
